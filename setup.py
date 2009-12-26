75325193535b867d9046d49e47db37873f9b19c8
Hanno Schlichting <hanno@hannosch.eu>
2009-12-26 17:31:02 +0000

    Prepare zope.mimetype 1.2.0.

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ from setuptools import setup, find_packages
 def read(*rnames):
     return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
 
-version = '1.2.0dev'
+version = '1.2.0'
 
 setup(name='zope.mimetype',
       version=version,
