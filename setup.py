9b5e93b2ae42e051222852390338c6f77fb59785
Tres Seaver <tseaver@palladion.com>
2010-05-06 13:23:33 +0000

    Conform to repository policy.

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 ##############################################################################
 #
-# Copyright (c) 2006 Zope Corporation and Contributors.
+# Copyright (c) 2006 Zope Foundation and Contributors.
 # All Rights Reserved.
 #
 # This software is subject to the provisions of the Zope Public License,
@@ -30,7 +30,7 @@ version = '1.2.1dev'
 
 setup(name='zope.mimetype',
       version=version,
-      author='Zope Corporation and Contributors',
+      author='Zope Foundation and Contributors',
       author_email='zope-dev@zope.org',
       description = "A simple package for working with MIME content types",
       long_description=(
