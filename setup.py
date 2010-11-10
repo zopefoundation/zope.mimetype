b7da41c2406b1320b2dcb0d6f1b0edb93fe900fd
Michael Howitz <mh@gocept.com>
2010-11-10 20:06:38 +0000

    Back to development: 1.3.2

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ from setuptools import setup, find_packages
 def read(*rnames):
     return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
 
-version = '1.3.1'
+version = '1.3.2dev'
 
 setup(name='zope.mimetype',
       version=version,
